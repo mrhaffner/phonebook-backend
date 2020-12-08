7b35bb8b8aa0916d9d8a243df2340e7686fb696d
Matt Haffner <mrhaffner13@gmail.com>
2020-12-07 20:15:10 -0600

    added build of react frontend

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('person', function (req, res) { return JSON.stringify(req.body)})
+
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
